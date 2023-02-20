1a737835f7a6c28c734c84861ec4d85f6c88a74c
Krista M <morales-k@outlook.com>
2023-02-20 18:54:13 -0500

    Add app files
    Added basic index and App files along with scss styling.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Styles/main.scss";
+
+const App = () =>{
+    return (
+        <h1>This is an React app that uses Sass, built without CRA!</h1>
+    )
+}
+
+export default App;
