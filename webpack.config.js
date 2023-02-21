c120634541f40f14e8f9d09b3dc7a9295771e0bd
Krista M <morales-k@outlook.com>
2023-02-20 21:29:22 -0500

    Fixed duplicated style tags issue
    Added MiniCSSExtractPlugin. Set modules false to avoid name obfuscation.

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,57 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './index.js',
-  mode: 'development',
+  entry: "./index.js",
+  mode: "development",
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'main.js',
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js",
   },
-  target: 'web',
+  target: "web",
   devServer: {
-    port: '3000',
+    port: "3000",
     static: {
-      directory: path.join(__dirname, './public')
+      directory: path.join(__dirname, "./public")
   },
     open: true,
     hot: true,
     liveReload: true,
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.json', '.scss'],
+    extensions: [".js", ".jsx", ".json", ".scss"],
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/, 
         exclude: /node_modules/, 
-        use: ['babel-loader'], 
+        use: ["babel-loader"], 
       },
       {
         test: /\.(s(a|c)ss)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-     }
+        use: [
+          MiniCSSExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: false,
+              sourceMap: true,
+            }
+          },
+          "sass-loader"
+        ]
+      }
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html')
+      template: path.join(__dirname, "public", "index.html")
+    }),
+    new MiniCSSExtractPlugin({
+      filename: "style.css",
+      chunkFilename: "[name].css"
     })
   ]
 };
